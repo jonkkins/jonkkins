a5bbe3810ce3c32d86ea548dbaa0aa3c2020421b
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-01-22 22:50:55 +0800

    Update login.py
    Ultrabasic working noob login. No database
    yet.

--- modules/login/src/login.py
+++ modules/login/src/login.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import request
+import bcrypt
 
 app = Flask(__name__)
 
-@app.route('/')
+# Of course, just a test code.
+username = 'ghabxph'
+
+# Password: stonkestpw
+password = b'$2b$12$Uc7LQULxOIJEkoGmSGzmPeMY1wNYgw9Upk2EZ3OTyeNA2VUIO6kuG'
+
+
+@app.route('/login', methods=['POST'])
 def hello_world():
-    return 'Hello, World!'
+    valid_username = request.form['username'].lower() == username.lower()
+    valid_password = bcrypt.hashpw(request.form['password'].encode('utf-8'), password) == password
+    if valid_username and valid_password:
+        return 'valid username and password heh'
+    return 'Invalid username and/or password'
