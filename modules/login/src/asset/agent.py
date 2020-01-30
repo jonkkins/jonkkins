297c8a893bb1d7aa2eee253eddfb203859c79049
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-01-30 21:22:20 +0800

    Update agent.py
    Agent is now able to login.
    Note: Just login...

--- modules/login/src/asset/agent.py
+++ modules/login/src/asset/agent.py
@@ -9,62 +9,71 @@ import time
 # Functions ------------------------------------------
 def get_password(password_file):
     with open(password_file, 'r') as password_file:
-        return password_file.read()
+        return password_file.read().strip()
 
 
 def set_password(password_file, new_password):
     with open(password_file, 'w') as password_file:
         password_file.write(new_password)
-
-
 # End of functions -----------------------------------
 
-# Check if required parameters are set
-if len(sys.argv) < 5:
-    print('Usage:')
-    print('%s schema host name password-path')
-    print('schema         - Either http or https')
-    print('host           - Domain / Hostname / IP of master')
-    print('name           - Name of agent')
-    print('password-path  - Path to password file')
-    exit(-1)
+try:
+    # Check if required parameters are set
+    if len(sys.argv) < 5:
+        print('Usage:')
+        print('%s schema host name password-path')
+        print('schema         - Either http or https')
+        print('host           - Domain / Hostname / IP of master')
+        print('name           - Name of agent')
+        print('password-path  - Path to password file')
+        exit(-1)
 
-# Initial variables set from command line
-schema = sys.argv[1]
-url = sys.argv[2]
-name = sys.argv[3]
-password_file = sys.argv[4]
+    # Initial variables set from command line
+    schema = sys.argv[1]
+    url = sys.argv[2]
+    name = sys.argv[3]
+    password_file = sys.argv[4]
 
-# URLs
-base_url = '%s://%s' % (schema, url)
+    # URLs
+    base_url = '%s://%s' % (schema, url)
+    login_url = base_url + '/login-agent'
 
-# Attempts to login for 5 times, then exits itself if login has failed for 5 consecutive times.
-login_attempt = 0
-while login_attempt < 5:
-    # Performs basic login
-    res = requests.post(base_url + '/login-slave',
-                        data={"name": name, "password": get_password(password_file)})
+    # Attempts to login for 5 times, then exits itself if login has failed for 5 consecutive times.
+    login_attempt = 0
+    while login_attempt < 5:
 
-    # Checks if our login request was accepted
-    if res.status_code == 200:
+        data = {"name": name, "password": get_password(password_file)}
 
-        # Resets login attempt
-        login_attempt = 0
+        print('Logging in to master: ' + login_url)
 
-        # Updates password (random password)
-        set_password(password_file, json.loads(res.text)['new_password'])
+        # Performs basic login
+        res = requests.post(login_url, data=data)
 
-        # Program loop that receives command from the master
-        while True:
-            # No code yet ...
+        # Checks if our login request was accepted
+        if res.status_code == 200:
+            print('Agent has been connected.')
 
-            # Sleeps for 1 second.
-            time.sleep(1)
+            # Resets login attempt
+            login_attempt = 0
 
-    print('Error: ' + json.loads(res.text)['msg'])
+            # Updates password (random password)
+            set_password(password_file, json.loads(res.text)['new_password'])
 
-    # Increment login attempt
-    login_attempt = login_attempt + 1
+            # Program loop that receives command from the master
+            while True:
+                # No code yet ...
 
-    # Sleeps for 5 seconds to reattempt login
-    time.sleep(5)
+                # Sleeps for 1 second.
+                time.sleep(1)
+
+        print('Error: ' + json.loads(res.text)['msg'])
+
+        # Increment login attempt
+        login_attempt = login_attempt + 1
+
+        # Sleeps for 5 seconds to reattempt login
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    print('')
+    print('Now closing the agent...')
