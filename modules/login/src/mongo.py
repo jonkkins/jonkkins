1e5db5bf99ab02961135a9e5612aaa0aaccdc45a
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-01-30 15:27:46 +0800

    Update mongo.py

--- modules/login/src/mongo.py
+++ modules/login/src/mongo.py
@@ -3,8 +3,30 @@ from config import config
 import urllib.parse
 
 
+class Mongo:
+
+    __conn__ = None
+    __DB_NAME__ = 'login'
+
+    @staticmethod
+    def conn():
+        username = urllib.parse.quote_plus(config()['db']['username'])
+        password = urllib.parse.quote_plus(config()['db']['password'])
+        host = config()['db']['host']
+
+        if Mongo.__conn__ is None:
+            Mongo.__conn__ = MongoClient('mongodb://%s:%s@%s' % (username, password, host))
+
+        return Mongo.__conn__
+
+    @staticmethod
+    def db():
+        return Mongo.conn()[config()['db']['db_prefix'] + Mongo.__DB_NAME__]
+
+
 def mongo():
-    username = urllib.parse.quote_plus(config()['db']['username'])
-    password = urllib.parse.quote_plus(config()['db']['password'])
-    host = config()['db']['host']
-    return MongoClient('mongodb://%s:%s@%s' % (username, password, host))
+    return Mongo.conn()
+
+
+def db():
+    return Mongo.db()
