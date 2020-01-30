f6f6b85b5ade12d2949a613ad0948ddc316d9fc2
Gabriel Lucernas Pascual <11975597+ghabxph@users.noreply.github.com>
2020-01-30 15:28:18 +0800

    Login is done.

--- modules/login/src/login.py
+++ modules/login/src/login.py
@@ -1,54 +1,29 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response
 from jwcrypto import jwt, jwk, jws
-from mongo import mongo
+from mongo import db
+from config import config
+import json
 import bcrypt
 import time
 
+# Essential Instances
 app = Flask(__name__)
-
-# Of course, just a test code.
-username = 'ghabxph'
-
-# Password: stonkestpw
-password = b'$2b$12$Uc7LQULxOIJEkoGmSGzmPeMY1wNYgw9Upk2EZ3OTyeNA2VUIO6kuG'
-
-# JWK
-key = jwk.JWK.from_json('{"k":"ttaVli605F0FOfdpUUpm9sKGeBRb0ooN6Dwta6ydqdo","kty":"oct"}')
-key2 = jwk.JWK(generate='oct', size=256)
-
-@app.route('/')
-def test():
-    myclient = mongo()
-    mydb = myclient["mydatabase"]
-    return jsonify(myclient.list_database_names())
-
-    return bcrypt.hashpw(b'dedededede', bcrypt.gensalt())
-    # token = jwt.JWT(header={'alg': 'HS256'}, claims={
-    #     'usr': username,
-    #     'iat': int(time.time()),
-    #     'exp': int(time.time() + 86400)
-    # })
-    # token.make_signed_token(key)
-    #
-    # # token verification
-    # try:
-    #     jwt.JWT(jwt=token.serialize(), key=key)
-    #     return Response('{"msg":"Token is valid"}', mimetype='application/json'), 200
-    # except jws.InvalidJWSSignature:
-    #     return Response('{"msg":"Token is invalid"}', mimetype='application/json'), 403
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    valid_username = request.form['username'].lower() == username.lower()
-    valid_password = bcrypt.hashpw(request.form['password'].encode('utf-8'), password) == password
+    auto_create_default_user()
+    user = db()['users'].find_one({"username": request.form['username'].lower()})
+    valid_username = request.form['username'].lower() == user['username'].lower()
+    valid_password = bcrypt.hashpw(request.form['password'].encode('utf-8'), user['password']) == user['password']
+
     if valid_username and valid_password:
         token = jwt.JWT(header={'alg': 'HS256'}, claims={
-            'usr': username,
+            'usr': user['username'],
             'iat': int(time.time()),
             'exp': int(time.time() + 86400)
         })
-        token.make_signed_token(key)
+        token.make_signed_token(jwk.JWK.from_json(json.dumps(config()['auth']['jwk'])))
         return token.serialize(), 200
     return 'Invalid username and/or password', 403
 
@@ -61,33 +36,8 @@ def verify_token():
     except jws.InvalidJWSSignature:
         return Response('{"msg":"Token is invalid"}', mimetype='application/json'), 403
 
-"""
-Things to do:
-  Reference: https://jwcrypto.readthedocs.io/en/latest/jwt.html
- -- Once username and password matches the database (for now we put it on a variable),
-    then what we're going to do next is to generate a JWT that:
-      - Claims that 'you' are the logged-in user
-      - Kindly see JWT's standard claim. I think the standard claims are
-        just enough. No need to think of a customized claim
-      - Sign the jwt token, and create:
-        {'jwt': '<jwt here'}
 
--- On the front side:
-    - Once we receive the valid login jwt, we must store it somewhere.
-    - And then, on every request we made, put the jwt on Authorization: Bearer <jwt>
-
--- Lastly,
-    - Perform a check on header. It'll be in the middleware of every application that
-      requires logged-in user.
-    - Once the claim are determined to be correct, we can just use the data written here.
-      No need to get the data from database, since it is signed, then it is less likely
-      to be manipulated
-    - We can consider adding another layer (encryption), thus, making it JWE (Json Web
-      Token Encrypted)
-
--- Questions:
-    - Where to store the generated key?
-      - user:
-      - pass:
-      - loginKey
-"""
+def auto_create_default_user():
+    default_user = db()['users'].find_one({"username": "ghabxph"})
+    if not default_user:
+        db()['users'].insert_one({"username": "ghabxph", "password": bcrypt.hashpw(b'stonkestpw', bcrypt.gensalt())})
